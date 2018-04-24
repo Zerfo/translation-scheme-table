a20c2dee72cc031504fd2f68c128743b36212cbc
Nikolai <Zerfogame@yandex.ru>
2018-04-24 11:59:41 +0300

    changed readme.md and corrected the styles of the mute window

--- src/App.js
+++ src/App.js
@@ -61,10 +61,11 @@ class App extends Component {
       this.addOrEdit = addOrEdit;
       this.key = key;
   }
+
   render() {
     return (
       <div className="container">
-        <div className="btnAdd" onClick={() => this.showModalAddWindow("add", 0)}>Добавить</div>
+        <div className="btnAdd" onClick={() => this.showModalAddWindow("add")}>Добавить</div>
         <div className="table">
           <table>
             <tr>
@@ -77,7 +78,7 @@ class App extends Component {
                 return <tr><td onClick={ () => this.showModalAddWindow("edit", item.id)}>{item.nameTask}</td><td>{item.originalLang}</td><td>{item.translateLang.join(", ")}</td><td>{item.comment}</td></tr>
             }) : '' }
           </table>
-          { localStorage.getItem('1') === null ? <div className="infoNoneTask">Нет ни одной схемы, <a className="noneTask" onClick={() => this.showModalAddWindow("add", 0)}> добавить</a></div> : '' }
+          { localStorage.getItem('1') === null ? <div className="infoNoneTask">Нет ни одной схемы, <a className="noneTask" onClick={() => this.showModalAddWindow("add")}> добавить</a></div> : '' }
         </div>
         { this.state.isModalOpen ? <ModalWindow mapLang={this.mapLang} keyLS={this.key} add={this.addOrEdit === "add" ? "Добавить" : "Сохранить"} onShow={() => this.showTask()} onClose={() => this.showModalAddWindow()}/> : '' }
       </div>
